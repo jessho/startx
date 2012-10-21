8aa071dc0fc62e12e998e4f840406a5360c2ba1c
Jess Ho <jess.c.ho@gmail.com>
2012-10-21 12:19:22 -0700

    created instance of startx homepage. separated bootstrap_and_overrides.css into components

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startx::Application.config.secret_token = '97c21b4922e236f9fa6a4cf9b3b1a282c33da83605763ee1dd97dcb2c4e86cbf73722f29d0f14cd2f06fa4646c8ab03f1e8bae6bef159e7a8bb66979219b6e30'
